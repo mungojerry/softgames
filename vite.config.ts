f6ba1d84205513a68749cc693ba6387ff8f63162
Matt Folkard <matt@plasticmartians.co.uk>
2025-11-14 14:37:40 +0000

    Set base URL in vite.config.ts to a fixed path for consistent deployment

--- vite.config.ts
+++ vite.config.ts
@@ -1,7 +1,7 @@
 import { defineConfig } from "vite";
 
 export default defineConfig({
-  base: process.env.NODE_ENV === "production" ? "/softgames/" : "/",
+  base: "/softgames/",
   server: {
     port: 3000,
     open: true,
