5fdbab60100f5a6872beb5aba2669995c013b4d4
Matt Folkard <matt@plasticmartians.co.uk>
2025-11-14 14:25:37 +0000

    Refactor AceOfShadowsConfig for improved mobile layout ratios and card scale

--- src/games/aceofshadows/AceOfShadowsConfig.ts
+++ src/games/aceofshadows/AceOfShadowsConfig.ts
@@ -14,14 +14,14 @@ export class AceOfShadowsConfig {
 
   // Mobile layout ratios
   static readonly MOBILE_TOP_Y_RATIO = 0.4;
-  static readonly MOBILE_BOTTOM_ROW_Y_RATIO = 0.55;
-  static readonly MOBILE_BOTTOM_ROW2_Y_RATIO = 0.75;
+  static readonly MOBILE_BOTTOM_ROW_Y_RATIO = 0.6;
+  static readonly MOBILE_BOTTOM_ROW2_Y_RATIO = 0.8;
   static readonly MOBILE_GRID_SPACING_RATIO = 0.35;
 
   // Desktop layout
   static readonly DESKTOP_STACK_SPACING = 200;
 
   // Scale factors
-  static readonly MOBILE_CARD_SCALE = 0.4;
+  static readonly MOBILE_CARD_SCALE = 0.5;
   static readonly DESKTOP_CARD_SCALE = 1;
 }
